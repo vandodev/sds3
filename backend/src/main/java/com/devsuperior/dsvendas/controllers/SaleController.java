84f2c01a0692ae97818fe0324c7da614afb5c1d0
Evandro oliveira <vandoaparecido@hotmail.com>
2021-05-06 00:02:47 -0300

    Mostrando vendedores e somando suas vendas

--- backend/src/main/java/com/devsuperior/dsvendas/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsvendas/controllers/SaleController.java
@@ -1,5 +1,7 @@
 package com.devsuperior.dsvendas.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsvendas.dto.SaleDTO;
+import com.devsuperior.dsvendas.dto.SaleSumDTO;
 import com.devsuperior.dsvendas.service.SaleService;
 
 @RestController
@@ -22,6 +25,12 @@ public class SaleController {
 	@GetMapping
 	public ResponseEntity<Page<SaleDTO>> findAll(Pageable pageable) {
 		Page<SaleDTO> list = service.findAll(pageable);
+		return ResponseEntity.ok(list);
+	}
+	
+	@GetMapping(value ="/amount-by-seller")
+		public ResponseEntity<List<SaleSumDTO>> amountGroupeBySeller() {
+		List<SaleSumDTO> list = service.amountGroupeBySeller();
 		return ResponseEntity.ok(list);
 	}
 }
